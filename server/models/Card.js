c1bf62913591cb19e023e87a838f9c48ec150802
Pablo Chiappetti <pchiappetti@gmail.com>
2017-03-26 00:06:57 -0300

    relaciones en los modelos

--- server/models/Card.js
+++ server/models/Card.js
@@ -1,5 +1,17 @@
 import { Model } from 'objection'
+import path from 'path'
 
 export default class Card extends Model {
   static tableName = 'cards'
+
+  static relationMappings = {
+    game: {
+      relation: Model.BelongsToOneRelation,
+      modelClass: path.join(__dirname, '/Game'),
+      join: {
+        from: 'cards.game_id',
+        to: 'games.id'
+      }
+    }
+  }
 }
--- server/models/Game.js
+++ server/models/Game.js
@@ -1,5 +1,17 @@
 import { Model } from 'objection'
+import Card from './Card'
 
 export default class Game extends Model {
   static tableName = 'games'
+
+  static relationMappings = {
+    cards: {
+      relation: Model.HasManyRelation,
+      modelClass: Card,
+      join: {
+        from: 'games.id',
+        to: 'cards.game_id'
+      }
+    }
+  }
 }
